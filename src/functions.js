1dbf17e232b5dc45ff29cde2cb5f4436d830e631
Qoosim <ayinde.abdulghaniyy@gmail.com>
2021-11-18 23:54:40 +0100

    Add functionalities to make posts to the involvement API

--- src/functions.js
+++ src/functions.js
@@ -1,7 +1,7 @@
 const foodItemsDiv = document.getElementById('food-items');
 const mealCount = document.querySelector('#meal-count');
-const mealDetails = document.querySelector('.meal-details');
 const mealDetailsContent = document.querySelector('.meal-details-content');
+const closeBtn = document.querySelector('.close-btn');
 
 const appId = 'Fp12eL7c25FQ3gxVeMCZ';
 
@@ -35,7 +35,20 @@ const getMealList = async () => {
   });
 };
 
+const postComment = async (data) => {
+  const url = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/Fp12eL7c25FQ3gxVeMCZ/comments';
+  const response = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  });
+  return response.ok;
+};
+
 const mealModal = (meal) => {
+  console.log(meal);
   [meal] = meal;
   mealDetailsContent.innerHTML = `
     <h2 class = "recipe-title">${meal.strMeal}</h2>
@@ -44,14 +57,41 @@ const mealModal = (meal) => {
       <h3>Instructions:</h3>
       <p>${meal.strInstructions}</p>
     </div>
-    <div class = "meal-img">
+    <div class = "recipe-meal-img">
       <img src = "${meal.strMealThumb}" alt = "">
     </div>
+    <h3 class="m-3">Comments(2)</h3>
+    <div class="d-flex justify-content-center align-items-center">
+      <p class="me-3">date</p>
+      <p class="me-3">name</p>
+      <p>comment</p>
+    </div>
+    <h3 class="m-3">Add a comment</h3>
+    <form autocomplete="off" class="w-50 mx-auto">
+      <input type="text" class="form-control w-75 mx-auto mb-2" id="commentator" placeholder="Your name">
+      <textarea id="comment" name="comment" placeholder="Your comment..."></textarea>
+      <button type="button" class="btn btn-secondary commentBtn">Comment</button>
+    </form>
   `;
   mealDetailsContent.parentElement.classList.add('showComment');
+
+  const commentBtn = document.querySelector('.commentBtn');
+  commentBtn.addEventListener('click', () => {
+    const username = document.querySelector('#commentator').value;
+    const comment = document.querySelector('#comment').value;
+    const itemId = meal.idMeal;
+    const newData = {
+      item_id: itemId,
+      username,
+      comment,
+    };
+    postComment(newData);
+    document.querySelector('#commentator').value = '';
+    document.querySelector('#comment').value = '';
+  });
 };
 
-const getMealRecipe = async (e) => {
+const getMeal = async (e) => {
   e.preventDefault();
   if (e.target.classList.contains('comment-btn')) {
     const mealItem = e.target.parentElement.parentElement.parentElement;
@@ -61,7 +101,10 @@ const getMealRecipe = async (e) => {
   }
 };
 
-foodItemsDiv.addEventListener('click', getMealRecipe);
+foodItemsDiv.addEventListener('click', getMeal);
+closeBtn.addEventListener('click', () => {
+  mealDetailsContent.parentElement.classList.remove('showComment');
+});
 
 export {
   getMealList,
