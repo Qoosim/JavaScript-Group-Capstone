98a7336d5ef259eca31a9c14b19a256d3ba2697f
Cathella <ncathie214@gmail.com>
2021-11-18 08:28:35 +0300

    Add a meals counter, style elements

--- src/functions.js
+++ src/functions.js
@@ -1,6 +1,7 @@
-const displayMeals = (food) => {
-  const foodItemsDiv = document.getElementById('food-items');
+const foodItemsDiv = document.getElementById('food-items');
+const mealCount = document.querySelector('#meal-count');
 
+const displayMeals = (food) => {
   const foodDiv = document.createElement('div');
   foodDiv.className = 'col-lg-4';
   foodDiv.innerHTML = `
@@ -8,14 +9,15 @@ const displayMeals = (food) => {
         <div class="meal-img bg-light">
           <img src="${food.strMealThumb}" alt="Meal Image">
         </div>
-        <div class="meal-name">
-          <h3 class="fs-6 my-3">${food.strMeal}&nbsp;&nbsp;<i class="bi bi-heart" role="button"></i></h3>
+        <div class="meal-name d-flex pt-3 justify-content-center align-items-center">
+          <h3 class="fs-6 me-2 pt-1">${food.strMeal}</h3>
+          <i class="text-danger bi bi-heart" role="button"></i>
         </div>
         <div class="like-count">
-          <p>2 likes</p>
+          <small class="text-muted">2 Likes</small>
         </div>
-        <div class="comment">
-          <button class="btn btn-light">Comments</button>
+        <div class="comment mb-4 mt-2">
+          <button class="btn btn-warning rounded-pill px-4 shadow">Comments</button>
         </div>
       </div>
     `;
@@ -23,10 +25,11 @@ const displayMeals = (food) => {
 };
 
 const getMeals = async () => {
-  const url = 'https://www.themealdb.com/api/json/v1/1/search.php?f=b';
+  const url = 'https://www.themealdb.com/api/json/v1/1/filter.php?a=indian';
   const response = await fetch(url);
   const data = await response.json();
   data.meals.forEach((meal) => displayMeals(meal));
+  mealCount.textContent = data.meals.length;
 };
 
 export default getMeals;
